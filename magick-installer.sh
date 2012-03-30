d9158fd6d35e1ded9025237e1175675b305dfb30
Barry Allard <barry@barryallard.name>
2012-03-29 17:35:26 -0700

    major cleanup.  it needed it.

--- magick-installer.sh
+++ magick-installer.sh
@@ -1,20 +1,62 @@
 #!/bin/sh
+#
+
+#  magick_installer.sh    [ 0|1 [ /path/to/install/dir [ 0|1 ] ] ] 
+#                            ^            ^               ^
+#                            |            |               1 = build static, 0 = build shared (default)
+#                            |            |
+#                            |            change the installation path (default: /usr/local)
+#                            |
+#                            0 = dont ever use sudo, 1 = use sudo to install (default)
+#
+
+# ---- versions
+
+         libiconv_version=1.13.1
+         freetype_version=2.4.3
+     libpng_major_version=15
+           libpng_version=1.5.5
+             jpeg_version=8b
+          libtiff_version=3.9.4
+           libwmf_version=0.2.8.4
+             lcms_version=1.19
+      ghostscript_version=9.04
+ghostscript_fonts_version=8.11
+      imagemagick_version=6.6.7-0
+
+
+
+# ---- initial code
+
+use_sudo="${1:-1}"
+prefix="${2:-/usr/local}" 
+static="${3:-0}" 
+codec='-aes-256-cbc'
+
+config_opts=''
+
 set -e
 
-use_sudo=${1:-1} # call this script with 0 to not use sudo
-prefix="${2:-/usr/local}" # pass argument 2 of this script to change the installation prefix
-
 password_on_sudo="`openssl rand -base64 96`"
-password_salt_on_sudo="`openssl rand -hex 32'`"
+password_salt_on_sudo="`openssl rand -hex 32`"
+
+
+
+# ---- functions
+
+function header() {
+  echo "############################ ${@}"
+}
 
 function exit_trap() {
-  rm -f $build_root/sudopwd >/dev/null 2>&1
+  rm -f "${build_root}/.sudopwd" >/dev/null 2>&1
   exit $1
 }
 
 # Obviously incompatible with pin entry, tokens, etc.
 function save_sudo_password() {
-  if [ -n "$sudo_pwd" ];
+  if [ -n "${sudo_pwd}" ]
+  then
     echo 'Sudo password already saved.' >&2
     return
   fi
@@ -22,7 +64,8 @@ function save_sudo_password() {
   read -rsp 'Password :' sudo_pwd
 
   # Make sure it basically works. 
-  if ! echo $sudo_pwd | sudo -n -S id >/dev/null 2>&1 ; then
+  if ! echo "${sudo_pwd}" | sudo -n -S id >/dev/null 2>&1 
+  then
     echo 'Unable to sudo :(' >&2
     false
   fi
@@ -30,153 +73,140 @@ function save_sudo_password() {
   # Save it.
   umask_save="`umask`"
   umask 077
-  trap 'exit_trap $1' EXIT
-  echo $sudo_pwd | openssl enc -aes-256-cbc \
-                      -pass pass:$password_on_sudo \
-                      -S $password_salt_on_sudo \
-                      -a -out $build_root/sudopwd
-  umask $umask_space
+  trap 'exit_trap ${1}' EXIT
+  echo "${sudo_pwd}" | openssl enc "${codec}"          \
+                      -pass "pass:${password_on_sudo}" \
+                      -S "${password_salt_on_sudo}"    \
+                      -a -out "${build_root/.sudopwd}"
+  umask "${umask_space}"
 }
 
 function unattended_sudo() {
-  openssl enc -d -aes-256-cbc \
-      -pass pass:$password_on_sudo \
-      -S $password_salt_on_sudo \
-      -a -in $build_root/sudopwd \
-  | sudo -S -n "$@"
+  openssl enc -d "${codec}"            \
+      -pass "pass:${password_on_sudo}" \
+      -S "${password_salt_on_sudo}"    \
+      -a -in "${build_root}/sudopwd"   \
+  | sudo -S -n "${@}"
 }
 
 function _sudo() {
-  if [[ $use_sudo -eq 1 ]]; then
-    unattended_sudo "$@"
+  if [[ "${use_sudo}" -eq 1 ]]
+  then
+    unattended_sudo "${@}"
   else # dont call the program with sudo
-    prog="$1" ; shift
-    "$prog" "$@"
+    prog="${1}" ; shift
+    "${prog}" "${@}"
   fi
 }
 
-function download() {
-  url=$1
-  base=$(basename $1)
+function _download() {
+  url="${1}"
+  base="$(basename ${1})"
 
-  if [[ ! -e $base ]]; then
-    echo "curling $url"
-    curl -O -L $url
+  if [ ! -e "${base}" ]
+  then
+    echo "curling ${url}"
+    curl -LO "${url}"
   fi
 }
 
-mkdir magick-installer
-cd magick-installer
-build_root="`pwd`"
+function extract() {
+  tarball="${1}" 
+  default_cd_dir="`echo ${tarball} | sed 's/\.tar\..*$//'`"
+  cd_dir="${2:-${default_cd_dir}}" 
 
-download http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.13.1.tar.gz
-download http://nongnu.askapache.com/freetype/freetype-2.4.3.tar.gz
-download http://sourceforge.net/projects/libpng/files/libpng15/older-releases/1.5.5/libpng-1.5.5.tar.gz
-download http://www.imagemagick.org/download/delegates/jpegsrc.v8b.tar.gz
-download http://download.osgeo.org/libtiff/tiff-3.9.4.tar.gz
-download http://voxel.dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.2.8.4.tar.gz
-download http://downloads.sourceforge.net/project/lcms/lcms/1.19/lcms-1.19.tar.gz
-download http://sourceforge.net/projects/ghostscript/files/GPL%20Ghostscript/9.04/ghostscript-9.04.tar.gz
-download http://voxel.dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
-download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.6.7-0.tar.gz
+  header "${default_cd_dir}"
+  test -d "${cd_dir}" || tar ${tar_opts}xf "${tarball}" 
+  pushd "${cd_dir}" >/dev/null
+}
 
+function build() {
+  ./configure --prefix="${prefix}" ${config_opts} ${@}
+  make clean
+  make
+  _sudo make install
+  popd >/dev/null
+}
 
-tar xzvf libiconv-1.13.1.tar.gz
-cd libiconv-1.13.1
-cd libcharset
-./configure --prefix="$prefix"
-make
-_sudo make install
-cd ../..
-
-tar xzvf freetype-2.4.3.tar.gz
-cd freetype-2.4.3
-./configure --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
-
-tar xzvf libpng-1.5.5.tar.gz
-cd libpng-1.5.5
-./configure --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
+function extract_build() {
+  extract "${1}" ; shift
+  build "${@}"
+}
 
 
-tar xzvf jpegsrc.v8b.tar.gz
-cd jpeg-8b
-ln -s -f `which glibtool` ./libtool
+# ---- main
+
+build_root=/tmp/magick-installer
+mkdir -p "${build_root}"
+pushd "${build_root}" >/dev/null
+
+
+# ---- downloads
+
+_download http://ftp.gnu.org/pub/gnu/libiconv/libiconv-${libiconv_version}.tar.gz
+_download http://nongnu.askapache.com/freetype/freetype-${freetype_version}.tar.gz
+_download http://sourceforge.net/projects/libpng/files/libpng${libpng_major_version}/older-releases/${libpng_version}/libpng-${libpng_version}.tar.gz
+_download http://www.imagemagick.org/download/delegates/jpegsrc.v${jpeg_version}.tar.gz
+_download http://download.osgeo.org/libtiff/tiff-${libtiff_version}.tar.gz
+_download http://voxel.dl.sourceforge.net/project/wvware/libwmf/${libwmf_version}/libwmf-${libwmf_version}.tar.gz
+_download http://downloads.sourceforge.net/project/lcms/lcms/${lcms_version}/lcms-${lcms_version}.tar.gz
+_download http://sourceforge.net/projects/ghostscript/files/GPL%20Ghostscript/${ghostscript_version}/ghostscript-${ghostscript_version}.tar.gz
+_download http://voxel.dl.sourceforge.net/project/gs-fonts/gs-fonts/${ghostscript_fonts_version}%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-${ghostscript_fonts_version}.tar.gz
+_download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-${imagemagick_version}.tar.gz
+
+
+# ---- build and install
+
+extract libiconv-${libiconv_version}.tar.gz libiconv-${libiconv_version}/libcharset
+build
+
+extract_build freetype-${freetype_version}.tar.gz
+
+extract_build libpng-${libpng_version}.tar.gz 
+
+extract jpegsrc.v${jpeg_version}.tar.gz jpeg-${jpeg_version}
+glibtool="`which glibtool 2>/dev/null`"
+test -x "${glibtool}"
+ln -sf "${glibtool}" ./libtool
 export MACOSX_DEPLOYMENT_TARGET=10.7
-./configure --enable-shared --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
+build
 
+extract_build tiff-${libtiff_version}.tar.gz 
 
-tar xzvf tiff-3.9.4.tar.gz
-cd tiff-3.9.4
-./configure --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
+extract_build libwmf-${libwmf_version}.tar.gz 
 
+extract_build lcms-${lcms_version}.tar.gz
 
-tar xzvf libwmf-0.2.8.4.tar.gz
-cd libwmf-0.2.8.4
-./configure --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
+extract_build ghostscript-${ghostscript_version}.tar.gz 
 
+extract ghostscript-fonts-std-${ghostscript_fonts_version}.tar.gz fonts
+_sudo mkdir -p "${prefix}/share/ghostscript/fonts"
+_sudo mv -f * "${prefix}/share/ghostscript/fonts"
+popd >/dev/null
 
-tar xzvf lcms-1.19.tar.gz
-cd lcms-1.19
-./configure --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
-
-
-tar zxvf ghostscript-9.04.tar.gz
-cd ghostscript-9.04
-./configure  --prefix="$prefix"
-make clean
-make
-_sudo make install
-cd ..
-
-
-tar zxvf ghostscript-fonts-std-8.11.tar.gz
-_sudo mkdir -p /usr/local/share/ghostscript/fonts
-_sudo mv -f fonts/* /usr/local/share/ghostscript/fonts
-
-
-tar xzvf ImageMagick-6.6.7-0.tar.gz
-cd ImageMagick-6.6.7-0
 export CPPFLAGS=-I"$prefix/include"
 export LDFLAGS=-L"$prefix/lib"
-./configure --prefix="$prefix" \
-            --disable-static \
-            --without-fontconfig \
-            --with-modules \
-            --without-perl \
-            --without-magick-plus-plus \
-            --with-quantum-depth=8 \
-            --with-gs-font-dir="$prefix/share/ghostscript/fonts" \
-            --disable-openmp
-make clean
-make
-_sudo make install
-cd ..
+extract_build ImageMagick-${imagemagick_version}.tar.gz                \
+                                                                       \
+                --disable-static                                       \
+                --without-fontconfig                                   \
+                --with-modules                                         \
+                --without-perl                                         \
+                --without-magick-plus-plus                             \
+                --with-quantum-depth=8                                 \
+                --with-gs-font-dir="${prefix}/share/ghostscript/fonts" \
+                --disable-openmp                                       \
 
-cd ..
-rm -Rf magick-installer
+
+
+
+# ---- escape build dir
+
+popd >/dev/null
+rm -Rf "${build_root}"
+
+
+
+# ---- finished
 
 echo "ImageMagick successfully installed!"
