7a714591964013657b7edafd472a038f8e497f51
Geoff Evans <gbeevans@me.com>
2012-06-19 13:42:44 +1200

    more of the same

--- magick-installer.sh
+++ magick-installer.sh
@@ -121,7 +121,7 @@ function extract() {
 }
 function extractgs() {
   tarball="${1}" 
-  default_cd_dir="`echo ${tarball} | sed 's/\.tgz\..*$//'`"
+  default_cd_dir="`echo ghostscript-${ghostscript_version}.tgz`"
   cd_dir="${2:-${default_cd_dir}}" 
 
   header "${default_cd_dir}"
